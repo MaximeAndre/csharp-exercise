32a3413c355b174b908a72c7fb0605b75a6e6ec8
=Maxime Andre <maxime.andre28@hotmail.fr>
2021-12-04 18:23:13 +0100

    Create UserInfo Entity in the Domain structure.

---
+++ CSharpExercise/src/Domain/Entities/UserInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace CSharpExercise.src.Domain.Entities
+{
+    public class UserInfo
+    {
+        public int Id { get; set; }
+        [JsonIgnore]
+        public string? Login { get; set; }
+        [JsonIgnore]
+        public string? Password { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Email { get; set; }
+    }
+}
